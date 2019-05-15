86df0a539d698ca7a5a27cf0cf869dc7d120c0c2
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2019-05-15 12:26:05 +0100

    refactored image upload and added upload directory

--- upload.js
+++ upload.js
@@ -1,20 +1,9 @@
-// var cloudinary = require('cloudinary').v2;
-
-// require('dotenv').config();
-
-// cloudinary.config({ 
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.API_KEY, 
-//     api_secret: process.env.API_SECRET 
-//   });
-
-
 var cloud = require('./utils/cloudinary');
 //Image Upload
 
-let uploadImage = (_cloudinary, image_uri, friendly_name) => {
-    _cloudinary.uploader.upload(image_uri, {public_id:friendly_name},
+let uploadImage = (_cloudinary, image_uri, folder, public_id) => {
+    _cloudinary.uploader.upload(image_uri, {folder, public_id },
         (error, result) => { console.log(result, error) });
 }
 
-uploadImage(cloud, "images/nodejs.png", "john");
+uploadImage(cloud, "images/nodejs.png", 'profile_pix', "sade");
